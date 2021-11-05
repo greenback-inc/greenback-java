0467deebfdaf19334fab05e53f89bc2b453271eb
Joe Lauer <joe@lauer.bz>
2021-11-05 13:02:40 -0400

    Add support for querying transactions by archetype

--- greenback-kit-core/src/main/java/com/greenback/kit/model/Document.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/Document.java
@@ -1,10 +1,12 @@
 package com.greenback.kit.model;
 
 import java.util.Map;
+import java.util.Set;
 
 public class Document extends GreenbackObject {
  
     protected String referenceId;
+    protected Set<DocumentFlag> flags;
     protected Map<String,String> attributes;
 
     public String getReferenceId() {
@@ -13,6 +15,14 @@ public class Document extends GreenbackObject {
 
     public void setReferenceId(String referenceId) {
         this.referenceId = referenceId;
+    }
+
+    public Set<DocumentFlag> getFlags() {
+        return flags;
+    }
+
+    public void setFlags(Set<DocumentFlag> flags) {
+        this.flags = flags;
     }
 
     public Map<String, String> getAttributes() {
--- greenback-kit-core/src/main/java/com/greenback/kit/model/TransactionQuery.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/TransactionQuery.java
@@ -5,12 +5,27 @@ import java.time.Instant;
 
 public class TransactionQuery extends DocumentQuery<TransactionQuery> {
     
+    protected Iterable<TransactionArchetype> archetypes;
     protected Iterable<TransactionType> types;
     protected String query;
     protected Instant minTransactedAt;
     protected Instant maxTransactedAt;
     protected Boolean descending;
 
+    public Iterable<TransactionArchetype> getArchetypes() {
+        return archetypes;
+    }
+
+    public TransactionQuery setArchetypes(Iterable<TransactionArchetype> archetypes) {
+        this.archetypes = archetypes;
+        return this;
+    }
+
+    public TransactionQuery setArchetypes(TransactionArchetype... archetypes) {
+        this.archetypes = toIterable(archetypes);
+        return this;
+    }
+    
     public Iterable<TransactionType> getTypes() {
         return this.types;
     }
