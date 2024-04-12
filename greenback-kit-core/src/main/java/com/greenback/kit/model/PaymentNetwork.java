e7b0249feb16203a9e5634e264698dbd94562d3a
Jared Barrus <jbarrus@greenback.com>
2024-04-12 13:33:32 -0400

    GBA-1445: SEPA Payment Network (Single Euro Payments Area)

--- greenback-kit-core/src/main/java/com/greenback/kit/model/PaymentNetwork.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/PaymentNetwork.java
@@ -5,6 +5,9 @@ public enum PaymentNetwork {
     VISA,
     MASTERCARD,
     AMEX,
-    DISCOVER;
+    DISCOVER,
+    
+    // Single Euro Payments Area (https://www.ecb.europa.eu/paym/integration/retail/sepa/html/index.en.html)
+    SEPA;
     
 }
